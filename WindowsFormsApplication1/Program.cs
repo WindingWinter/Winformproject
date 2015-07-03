e27f101776d3a325bd67f09136753ae1cd4ce0d3
nsoonhui <soonhui.ngu@gmail.com>
2015-07-03 16:14:10 +0800

    Sh also wants to work on it

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -26,7 +26,7 @@ namespace WindowsFormsApplication1
 
 
         /// <summary>
-        /// Now start to do a branch 1
+        /// Now start to do a branch 1, SH also wants to work on it. 
         /// </summary>
         private static void Branch1()
         {
